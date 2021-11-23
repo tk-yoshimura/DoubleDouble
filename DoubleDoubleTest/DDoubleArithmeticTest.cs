be3b05f0e507f43e09c0e5eaab17c28de409a2b2
tk-yoshimura <yoshimura8192@gmail.com>
2021-11-23 15:45:42 +0900

    Update DDoubleArithmeticTest.cs

--- DoubleDoubleTest/DDoubleArithmeticTest.cs
+++ DoubleDoubleTest/DDoubleArithmeticTest.cs
@@ -47,5 +47,65 @@ namespace DoubleDoubleTest {
             Assert.AreEqual(1 / double.MinValue, (double)rcp_mval, double.Epsilon, nameof(rcp_mval));
             Assert.IsTrue(double.IsInfinity((double)rcp_eps) && Math.CopySign(1, (double)(rcp_eps)) > 0, nameof(rcp_eps));
         }
+
+        [TestMethod]
+        public void AddTest() {
+            foreach (int n in new int[] { 7, 13, 17 }) {
+                ddouble u = ddouble.Rcp(n);
+                double v = 1d / n;
+
+                ddouble su = 0;
+                double sv = 0;
+
+                for (int i = 0; i < n * 100; i++) {
+                    su += u;
+                    sv += v;
+                }
+
+                Assert.IsTrue(Math.Abs(100 - (double)su) <= Math.Abs(100 - sv));
+                Assert.AreEqual(100, (double)su, 1e-30);
+            }
+        }
+
+        [TestMethod]
+        public void SubTest() {
+            foreach (int n in new int[] { 7, 13, 17 }) {
+                ddouble u = ddouble.Rcp(n);
+                double v = 1d / n;
+
+                ddouble su = 0;
+                double sv = 0;
+
+                for (int i = 0; i < n * 100; i++) {
+                    su -= u;
+                    sv -= v;
+                }
+
+                Assert.IsTrue(Math.Abs(-100 - (double)su) <= Math.Abs(-100 - sv));
+                Assert.AreEqual(-100, (double)su, 1e-30);
+            }
+        }
+
+        [TestMethod]
+        public void MulTest() {
+            foreach (int n in new int[] { -7, -13, -17, -257, 7, 13, 17, 257 }) {
+                ddouble u = (ddouble.Rcp(n)) * n;
+                double v = (1d / n) * n;
+
+                Assert.IsTrue(Math.Abs((double)(1 - u)) <= Math.Abs(1 - v));
+                Assert.AreEqual(0, (double)(1 - u), 1e-30);
+            }
+        }
+
+        [TestMethod]
+        public void DivTest() {
+            foreach (int m in new int[] { -1, -5, -9, 1, 5, 9 }) {
+                foreach (int n in new int[] { -7, -13, -17, -257, 7, 13, 17, 257 }) {
+                    ddouble u = (m / (ddouble)n) * n;
+
+                    Assert.AreEqual(0, (double)(m - u), 1e-30);
+                }
+            }
+        }
     }
 }
