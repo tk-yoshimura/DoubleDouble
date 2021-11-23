c85fb0fa7ef4d4614bb1c32e3f5ea2a2c3b61a2b
tk-yoshimura <yoshimura8192@gmail.com>
2021-11-23 22:21:32 +0900

    Create DDoubleCastTest.cs

---
+++ DoubleDoubleTest/DDoubleCastTest.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DoubleDouble;
+using System;
+
+namespace DoubleDoubleTest {
+    [TestClass]
+    public class DDoubleCastTest {
+    }
+}
