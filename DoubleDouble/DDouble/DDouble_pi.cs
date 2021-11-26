70242ce414d9fdf8421851a5004067374b405b48
tk-yoshimura <yoshimura8192@gmail.com>
2021-11-27 03:55:44 +0900

    + pi

---
+++ DoubleDouble/DDouble/DDouble_pi.cs
@@ -0,0 +1,30 @@
+﻿namespace DoubleDouble {
+    public partial struct ddouble {
+        public static readonly ddouble PI = GeneratePI();
+        public static readonly ddouble RcpPI = Rcp(PI);
+
+        private static ddouble GeneratePI() {
+            ddouble a = 1;
+            ddouble b = Ldexp(Sqrt(2), -1);
+            ddouble t = Ldexp(1, -2);
+            ddouble p = 1;
+
+            for (long i = 1; i <= 128; i *= 2) {
+                ddouble a_next = (a + b) / 2;
+                ddouble b_next = Sqrt(a * b);
+                ddouble t_next = t - p * (a - a_next) * (a - a_next);
+                ddouble p_next = p * 2;
+
+                a = a_next;
+                b = b_next;
+                t = t_next;
+                p = p_next;
+            }
+
+            ddouble c = a + b;
+            ddouble y = c * c / (t * 4);
+
+            return y;
+        }
+    }
+}
--- DoubleDoubleTest/DDoubleSequenceTest.cs
+++ DoubleDoubleTest/DDoubleSequenceTest.cs
@@ -4,20 +4,29 @@ using System;
 
 namespace DoubleDoubleTest {
     [TestClass]
-    public class DDoubleSequenceTest {
+    public class DDoubleConstantTest {
         [TestMethod]
-        public void TaylorTest() {
-            for (int n = 0; n < ddouble.TaylorSequence.Count; n++) {
-                Console.WriteLine($"1/{n}! = {ddouble.TaylorSequence[n]}");
-            }
+        public void ETest() {
+            ddouble expected = (ddouble)"2.71828182845904523536028747135266250";
+            ddouble actual = ddouble.E;
+            ddouble error = expected - actual;
 
-            Assert.AreEqual(1, ddouble.TaylorSequence[0]);
-            Assert.AreEqual(1, ddouble.TaylorSequence[1]);
-            Assert.AreEqual(ddouble.Rcp(2), ddouble.TaylorSequence[2]);
-            Assert.AreEqual(ddouble.Rcp(6), ddouble.TaylorSequence[3]);
-            Assert.AreEqual(ddouble.Rcp(24), ddouble.TaylorSequence[4]);
-            Assert.AreEqual(ddouble.Rcp(120), ddouble.TaylorSequence[5]);
-            Assert.AreEqual(ddouble.Rcp(720), ddouble.TaylorSequence[6]);
+            Console.WriteLine(actual);
+            Console.WriteLine(error);
+
+            Assert.IsTrue(ddouble.Abs(error) < 1e-31);
+        }
+
+        [TestMethod]
+        public void PITest() {
+            ddouble expected = (ddouble)"3.14159265358979323846264338327950288";
+            ddouble actual = ddouble.PI;
+            ddouble error = expected - actual;
+
+            Console.WriteLine(actual);
+            Console.WriteLine(error);
+
+            Assert.IsTrue(ddouble.Abs(error) < 1e-31);
         }
     }
 }
