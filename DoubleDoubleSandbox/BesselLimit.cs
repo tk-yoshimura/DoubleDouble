4338b9a898ce494c8294941620ef3dc9bcd2dfb4
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-02 17:00:11 +0900

    sandbox bessel

---
+++ DoubleDoubleSandbox/BesselLimit.cs
@@ -0,0 +1,59 @@
+﻿using DoubleDouble;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleDoubleSandbox {
+    internal static class BesselLimit {
+
+        public static (ddouble y, int terms) BesselJ(ddouble nu, ddouble z, int max_terms = 64) {
+            BesselLimitCoef table = new BesselLimitCoef(nu);
+
+            ddouble v = 1 / z;
+            ddouble w = v * v;
+
+            ddouble x = 0, y = 0, p = 1, q = v;
+
+            int k = 0;
+            for (int conv_cnt = 0; k <= max_terms && conv_cnt < 2; k++, p *= w, q *= w) {
+                ddouble dx = p * table.Value(k * 2);
+                ddouble dy = q * table.Value(k * 2 + 1);
+
+                if ((k & 1) == 0) {
+                    ddouble x_next = x + dx;
+                    ddouble y_next = y + dy;
+
+                    if (x == x_next && y == y_next) {
+                        conv_cnt++;
+                    }
+
+                    x = x_next;
+                    y = y_next;
+                }
+                else {
+                    ddouble x_next = x - dx;
+                    ddouble y_next = y - dy;
+
+                    if (x == x_next && y == y_next) {
+                        conv_cnt++;
+                    }
+
+                    x = x_next;
+                    y = y_next;
+                }
+
+                if (k == max_terms) {
+                    return (ddouble.NaN, k);
+                }
+            }
+
+            ddouble omega = z - (2 * nu + 1) * ddouble.PI / 4;
+            ddouble m = x * ddouble.Cos(omega) - y * ddouble.Sin(omega);
+            ddouble t = m * ddouble.Sqrt(2 / (ddouble.PI * z));
+
+            return (t, k);
+        }
+    }
+}
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -1,68 +1,26 @@
 ﻿using DoubleDouble;
 using System;
+using System.IO;
 
 namespace DoubleDoubleSandbox {
     internal class Program {
         static void Main(string[] args) {
-            //using (StreamWriter sw = new StreamWriter("../../erfc_convergence_n.csv")) {
-            //
-            //    sw.WriteLine("z,f_init,f_init_prev,n,erfc(z)");
-            //
-            //    for (ddouble z = 1; z <= 10; z += 0.125d) {
-            //
-            //        int min_n = int.MaxValue;
-            //        ddouble min_f_init = ddouble.NaN;
-            //
-            //        for (ddouble f_init = 1d; f_init <= 1000; f_init += 1d / 8) {
-            //
-            //            ddouble erfc_prev = ddouble.NaN;
-            //            for (int n = 1; n <= 100; n++) {
-            //                ddouble erfc = ErfcContinuedFraction.Erfc(z, n, f_init);
-            //
-            //                if (erfc != erfc_prev) {
-            //                    erfc_prev = erfc;
-            //                }
-            //                else {
-            //                    if (min_n > n) {
-            //                        min_n = n;
-            //                        min_f_init = f_init;
-            //                    }
-            //                    break;
-            //                }
-            //            }
-            //        }
-            //
-            //        if(min_n < int.MaxValue){
-            //            ddouble erfc = ErfcContinuedFraction.Erfc(z, min_n - 1, min_f_init);
-            //
-            //            ddouble w = z * z;
-            //
-            //            ddouble f = 
-            //                (ddouble.Sqrt(25 + w * (440 + w * (488 + w * 16 * (10 + w))))
-            //                 - 5 + w * 4 * (1 + w))
-            //                / (20 + w * 8);
-            //
-            //            sw.WriteLine($"{z},{min_f_init},{f},{min_n - 1},{erfc}");
-            //            Console.WriteLine($"{z},{min_f_init},{f},{min_n - 1},{erfc}");
-            //        }
-            //    }
-            //}
+            using (StreamWriter sw = new StreamWriter("../../bessel_j.csv")) {
 
-            for (decimal v = 0.5m; v <= 6.25m; v += 1m / 16) {
-                ddouble x = (ddouble)v, x_dec = ddouble.BitDecrement(x), x_inc = ddouble.BitIncrement(x);
+                sw.WriteLine("nu,z,terms,y");
 
-                ddouble erfc_dec = ddouble.Erfc(x_dec);
-                ddouble erfc = ddouble.Erfc(x);
-                ddouble erfc_inc = ddouble.Erfc(x_inc);
+                for (double nu = -4d; nu <= 4d; nu += 0.125d) {
+                    for (double z = 1d; z <= 256d; z += 0.5d) {
+                        (ddouble y, int terms) = BesselLimit.BesselJ(nu, z);
 
-                Console.WriteLine($"erfc{x}");
-                Console.WriteLine(erfc_dec);
-                Console.WriteLine(erfc);
-                Console.WriteLine(erfc_inc);
+                        if (ddouble.IsNaN(y)) {
+                            continue;
+                        }
 
-                Console.WriteLine($"0x{FloatSplitter.Split(erfc_dec).mantissa:X14}");
-                Console.WriteLine($"0x{FloatSplitter.Split(erfc).mantissa:X14}");
-                Console.WriteLine($"0x{FloatSplitter.Split(erfc_inc).mantissa:X14}");
+                        sw.WriteLine($"{nu},{z},{terms},{y}");
+                        Console.WriteLine($"{nu},{z},{terms},{y}");
+                    }
+                }
             }
 
             Console.WriteLine("END");
