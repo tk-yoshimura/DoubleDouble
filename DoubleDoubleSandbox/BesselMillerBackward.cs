513b47ce1ab18758b312de9d91a1e71e6e886ef9
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-13 02:59:33 +0900

    sandbox bessely integer_nu

--- DoubleDoubleSandbox/BesselMillerBackward.cs
+++ DoubleDoubleSandbox/BesselMillerBackward.cs
@@ -103,6 +103,56 @@ namespace DoubleDoubleSandbox {
             }
         }
 
+        public static ddouble BesselY(int n, ddouble z, int m) {
+            if (m < 2 || (m & 1) != 0) {
+                throw new ArgumentOutOfRangeException(nameof(m));
+            }
+
+            if (n < 0) {
+                return ((n & 1) == 0) ? BesselY(-n, z, m) : -BesselY(-n, z, m);
+            }
+
+            ddouble m0 = 1e-256, m1 = ddouble.Zero, d = ddouble.Zero;
+            ddouble[] fs = new ddouble[m];
+            ddouble v = 1d / z;
+
+            for (int k = m; k >= 1; k--) {
+                if ((k & 1) == 0) {
+                    d += m0;
+                }
+
+                (m0, m1) = ((2 * k) * v * m0 - m1, m0);
+
+                fs[k - 1] = m0;
+            }
+
+            d = ddouble.Ldexp(d, 1) + m0;
+
+            ddouble[] gs = new ddouble[m / 2 + 1], xs = new ddouble[m];
+            gs[0] = (2 * (ddouble.Log(z / 2) + ddouble.EulerGamma)) / ddouble.PI;
+            gs[1] = 4 / ddouble.PI;
+            xs[0] = -2 / (z * ddouble.PI);
+            xs[1] = gs[0] - gs[1] / 2;
+
+            for (int k = 2; k < gs.Length; k++) {
+                gs[k] = -gs[k - 1] * (k - 1) / k; 
+            }
+            for (int k = 3; k < xs.Length; k+= 2) {
+                xs[k] = (gs[k / 2] - gs[k / 2 + 1]) / 2;
+            }
+
+            ddouble y0 = gs[0] * fs[0] + gs[1] * fs[2], y1 = xs[0] * fs[0] + xs[1] * fs[1];
+            for (int k = 4; k < fs.Length; k += 2) {
+                y0 += gs[k / 2] * fs[k];
+                y1 += xs[k - 1] * fs[k - 1];
+            }
+
+            y0 /= d;
+            y1 /= d;
+
+            return y0;
+        }
+
 
         public static (ddouble y, int m) BesselJ(int n, ddouble z, ddouble eps, int max_m = 512) {
             ddouble y_prev = ddouble.NaN, dy = ddouble.NaN, dy_prev = ddouble.NaN;
