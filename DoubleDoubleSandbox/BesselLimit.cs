8a21308a52df84ff0a16be34fe3d955a5cfaa768
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-22 00:37:13 +0900

    sandbox bessel

--- DoubleDoubleSandbox/BesselLimit.cs
+++ DoubleDoubleSandbox/BesselLimit.cs
@@ -5,7 +5,9 @@ using System.Linq;
 
 namespace DoubleDoubleSandbox {
     internal static class BesselLimit {
-        private static Dictionary<ddouble, CoefTable> coef_table = new();
+        private static Dictionary<ddouble, ACoefTable> a_table = new();
+        private static Dictionary<ddouble, JYCoefTable> jy_table = new();
+        private static Dictionary<ddouble, IKCoefTable> ik_table = new();
 
         public static (ddouble y, int terms) BesselJ(ddouble nu, ddouble x, int max_terms = 64) {
             (ddouble c, ddouble s, int terms) = BesselJYCoef(nu, x, max_terms);
@@ -32,7 +34,7 @@ namespace DoubleDoubleSandbox {
 
             ddouble t = c / ddouble.Sqrt(2 * ddouble.PI * x);
 
-            if (scale) {
+            if (!scale) {
                 t *= ddouble.Exp(x);
             }
 
@@ -44,7 +46,7 @@ namespace DoubleDoubleSandbox {
 
             ddouble t = c * ddouble.Sqrt(ddouble.PI / (2 * x));
 
-            if (scale) {
+            if (!scale) {
                 t *= ddouble.Exp(-x);
             }
 
@@ -52,24 +54,22 @@ namespace DoubleDoubleSandbox {
         }
 
         public static (ddouble s, ddouble t, int terms) BesselJYCoef(ddouble nu, ddouble x, int max_terms = 64) {
-            if (!coef_table.ContainsKey(nu)) {
-                coef_table.Add(nu, new CoefTable(nu));
+            if (!a_table.ContainsKey(nu)) {
+                a_table.Add(nu, new ACoefTable(nu));
+            }
+            if (!jy_table.ContainsKey(nu)) {
+                jy_table.Add(nu, new JYCoefTable(nu));
             }
 
-            CoefTable table = coef_table[nu];
-
-            ddouble squa_nu4 = 4d * nu * nu;
+            ACoefTable a = a_table[nu];
+            JYCoefTable c = jy_table[nu];
 
             ddouble v = 1d / x, v2 = v * v, v4 = v2 * v2;
             ddouble s = 0d, t = 0d, p = 1d, q = v;
 
-            static int square(int n) => checked(n * n);
-
             for (int k = 0; k <= max_terms; k++) {
-                ddouble ds = p * table.Value(k * 4) *
-                    (1d - v2 * (squa_nu4 - square(8 * k + 1)) * (squa_nu4 - square(8 * k + 3)) / (64 * (4 * k + 1) * (4 * k + 2)));
-                ddouble dt = q * table.Value(k * 4 + 1) *
-                    (1d - v2 * (squa_nu4 - square(8 * k + 3)) * (squa_nu4 - square(8 * k + 5)) / (64 * (4 * k + 2) * (4 * k + 3)));
+                ddouble ds = p * a[k * 4] * (1d - v2 * c[k].p0);
+                ddouble dt = q * a[k * 4 + 1] * (1d - v2 * c[k].p1);
 
                 ddouble s_next = s + ds;
                 ddouble t_next = t + dt;
@@ -88,22 +88,22 @@ namespace DoubleDoubleSandbox {
         }
 
         public static (ddouble r, int terms) BesselIKCoef(ddouble nu, ddouble x, bool sign_switch, int max_terms = 256) {
-            if (!coef_table.ContainsKey(nu)) {
-                coef_table.Add(nu, new CoefTable(nu));
+            if (!a_table.ContainsKey(nu)) {
+                a_table.Add(nu, new ACoefTable(nu));
+            }
+            if (!ik_table.ContainsKey(nu)) {
+                ik_table.Add(nu, new IKCoefTable(nu));
             }
 
-            CoefTable table = coef_table[nu];
-
-            ddouble squa_nu4 = 4d * nu * nu;
+            ACoefTable a = a_table[nu];
+            IKCoefTable c = ik_table[nu];
 
             ddouble v = 1d / x, v2 = v * v;
             ddouble r = 0d, u = 1d;
 
-            static int square(int n) => checked(n * n);
-
             for (int k = 0; k <= max_terms; k++) {
-                ddouble w = v * (squa_nu4 - square(4 * k + 1)) / (8 * (2 * k + 1));
-                ddouble dr = u * table.Value(k * 2) * (sign_switch ? (1d - w) : (1d + w));
+                ddouble w = v * c[k];
+                ddouble dr = u * a[k * 2] * (sign_switch ? (1d - w) : (1d + w));
 
                 ddouble r_next = r + dr;
 
@@ -118,11 +118,11 @@ namespace DoubleDoubleSandbox {
             return (ddouble.NaN, int.MaxValue);
         }
 
-        private class CoefTable {
+        private class ACoefTable {
             private readonly ddouble squa_nu4;
             private readonly List<ddouble> a_table = new();
 
-            public CoefTable(ddouble nu) {
+            public ACoefTable(ddouble nu) {
                 this.squa_nu4 = 4 * nu * nu;
 
                 ddouble a1 = (squa_nu4 - 1) / 8;
@@ -130,6 +130,8 @@ namespace DoubleDoubleSandbox {
                 this.a_table.Add(1d);
                 this.a_table.Add(a1);
             }
+
+            public ddouble this[int n] => Value(n);
 
             public ddouble Value(int n) {
                 if (n < 0) {
@@ -144,6 +146,69 @@ namespace DoubleDoubleSandbox {
                     ddouble a = a_table.Last() * (squa_nu4 - checked((2 * k - 1) * (2 * k - 1))) / checked(k * 8);
 
                     a_table.Add(a);
+                }
+
+                return a_table[n];
+            }
+        }
+
+        private class JYCoefTable {
+            private readonly ddouble squa_nu4;
+            private readonly List<(ddouble p0, ddouble p1)> a_table = new();
+
+            public JYCoefTable(ddouble nu) {
+                this.squa_nu4 = 4 * nu * nu;
+            }
+
+            public (ddouble p0, ddouble p1) this[int n] => Value(n);
+
+            public (ddouble p0, ddouble p1) Value(int n) {
+                if (n < 0) {
+                    throw new ArgumentOutOfRangeException(nameof(n));
+                }
+
+                if (n < a_table.Count) {
+                    return a_table[n];
+                }
+
+                static int square(int n) => checked(n * n);
+
+                for (int k = a_table.Count; k <= n; k++) {
+                    ddouble p0 = (squa_nu4 - square(8 * k + 1)) * (squa_nu4 - square(8 * k + 3)) / checked(64 * (4 * k + 1) * (4 * k + 2));
+                    ddouble p1 = (squa_nu4 - square(8 * k + 3)) * (squa_nu4 - square(8 * k + 5)) / checked(64 * (4 * k + 2) * (4 * k + 3));
+
+                    a_table.Add((p0, p1));
+                }
+
+                return a_table[n];
+            }
+        }
+
+        private class IKCoefTable {
+            private readonly ddouble squa_nu4;
+            private readonly List<ddouble> a_table = new();
+
+            public IKCoefTable(ddouble nu) {
+                this.squa_nu4 = 4 * nu * nu;
+            }
+
+            public ddouble this[int n] => Value(n);
+
+            public ddouble Value(int n) {
+                if (n < 0) {
+                    throw new ArgumentOutOfRangeException(nameof(n));
+                }
+
+                if (n < a_table.Count) {
+                    return a_table[n];
+                }
+
+                static int square(int n) => checked(n * n);
+
+                for (int k = a_table.Count; k <= n; k++) {
+                    ddouble p = (squa_nu4 - square(4 * k + 1)) / checked(8 * (2 * k + 1));
+
+                    a_table.Add(p);
                 }
 
                 return a_table[n];
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -5,99 +5,7 @@ using System.IO;
 namespace DoubleDoubleSandbox {
     internal class Program {
         static void Main(string[] args) {
-            for (double x = 1; x <= 35; x += 1) {
-                ddouble y = Sterling.Gamma(x);
-                ddouble r = (x <= 21.5d) ? ddouble.RoundMantissa(y, 99) : ddouble.RoundMantissa(y, 97);
 
-                Console.WriteLine($"{x},{y}");
-                Console.WriteLine($"{FloatSplitter.Split(y).mantissa:X14}");
-                Console.WriteLine($"{FloatSplitter.Split(r).mantissa:X14}");
-                //Console.WriteLine("");
-            }
-
-            for (double x = 1; x <= 35; x += 1) {
-                ddouble y = Sterling.Digamma(x);
-                ddouble e = -ddouble.EulerGamma + ddouble.HarmonicNumber((int)x - 1);
-
-                Console.WriteLine($"{x}");
-                Console.WriteLine($"{y}");
-                Console.WriteLine($"{e}");
-                Console.WriteLine($"{FloatSplitter.Split(y).mantissa:X14}");
-                Console.WriteLine($"{FloatSplitter.Split(e).mantissa:X14}");
-            }
-
-
-            //ddouble x = 2;
-            //
-            //for (ddouble nu = -8; nu <= 8d; nu += 0.25d) {
-            //    ddouble y = BesselMillerBackward.BesselJ(nu, x, 32);
-            //
-            //    Console.WriteLine($"besselJ({nu}, {x})={y}");
-            //}
-            //for (ddouble nu = -8; nu <= 8d; nu += 0.25d) {
-            //    ddouble y = BesselMillerBackward.BesselY(nu, x, 32);
-            //
-            //    Console.WriteLine($"besselY({nu}, {x})={y}");
-            //}
-            //for (ddouble nu = -8; nu <= 8d; nu += 0.25d) {
-            //    ddouble y = BesselMillerBackward.BesselI(nu, x, 32);
-            //
-            //    Console.WriteLine($"besselI({nu}, {x})={y}");
-            //}
-            //for (ddouble nu = 0; nu <= 8d; nu += 0.25d) {
-            //    ddouble y = BesselYoshidaPade.BesselK(nu, x);
-            //
-            //    Console.WriteLine($"besselK({nu}, {x})={y}");
-            //}
-
-            //using (StreamWriter sw = new StreamWriter("../../bessel_j_millerbw.csv")) {
-            //    sw.WriteLine("n,z,m,y");
-            //    for (int n = 0; n <= 4; n++) {
-            //        for (double z = 2d; z <= 40d; z += 0.25d) {
-            //            (ddouble y, int m) = BesselMillerBackward.BesselJ(n, z, eps: 1e-25);
-            //
-            //            sw.WriteLine($"{n},{z},{m},{y}");
-            //            Console.WriteLine($"{n},{z},{m},{y}");
-            //        }
-            //    }
-            //}
-            //
-            //using (StreamWriter sw = new StreamWriter("../../bessel_j_millerbw_2.csv")) {
-            //    sw.WriteLine("n,z,y");
-            //    for (int n = 0; n <= 8; n++) {
-            //        for (double z = 2d; z <= 40d; z += 0.25d) {
-            //            ddouble y = BesselMillerBackward.BesselJ(n, z);
-            //
-            //            sw.WriteLine($"{n},{z},{y}");
-            //            Console.WriteLine($"{n},{z},{y}");
-            //        }
-            //    }
-            //}
-            //
-            //
-            //BesselNearZero.CoefTable coef = new BesselNearZero.CoefTable(2.5);
-            //BesselNearZero.BesselJ(0, 40);
-            //
-            //coef.Value(16);
-            //
-            //using (StreamWriter sw = new StreamWriter("../../bessel_j.csv")) {
-            //    sw.WriteLine("nu,z,terms,y");
-            //    for (double nu = -4d; nu <= 4d; nu += 0.125d) {
-            //        for (double z = 1d; z <= 256d; z += 0.5d) {
-            //            (ddouble y, int terms) = BesselLimit.BesselJ(nu, z);
-            //
-            //            if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
-            //                continue;
-            //            }
-            //
-            //            sw.WriteLine($"{nu},{z},{terms},{y}");
-            //            Console.WriteLine($"{nu},{z},{terms},{y}");
-            //
-            //            break;
-            //        }
-            //    }
-            //}
-            //
             using (StreamWriter sw = new StreamWriter("../../bessel_j_nz.csv")) {
                 sw.WriteLine("nu,z,terms,y");
                 for (double nu = -4d; nu <= 4d; nu += 0.125d) {
@@ -150,60 +58,77 @@ namespace DoubleDoubleSandbox {
                 }
             }
 
-            //
-            //using (StreamWriter sw = new StreamWriter("../../bessel_y.csv")) {
-            //    sw.WriteLine("nu,z,terms,y");
-            //    for (double nu = -4d; nu <= 4d; nu += 0.125d) {
-            //        for (double z = 1d; z <= 256d; z += 0.5d) {
-            //            (ddouble y, int terms) = BesselLimit.BesselY(nu, z);
-            //
-            //            if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
-            //                continue;
-            //            }
-            //
-            //            sw.WriteLine($"{nu},{z},{terms},{y}");
-            //            Console.WriteLine($"{nu},{z},{terms},{y}");
-            //
-            //            break;
-            //        }
-            //    }
-            //}
-            //
-            //using (StreamWriter sw = new StreamWriter("../../bessel_i.csv")) {
-            //    sw.WriteLine("nu,z,terms,y");
-            //    for (double nu = -4d; nu <= 4d; nu += 0.125d) {
-            //        for (double z = 1d; z <= 256d; z += 0.5d) {
-            //            (ddouble y, int terms) = BesselLimit.BesselI(nu, z);
-            //
-            //            if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
-            //                continue;
-            //            }
-            //
-            //            sw.WriteLine($"{nu},{z},{terms},{y}");
-            //            Console.WriteLine($"{nu},{z},{terms},{y}");
-            //
-            //            break;
-            //        }
-            //    }
-            //}
-            //
-            //using (StreamWriter sw = new StreamWriter("../../bessel_k.csv")) {
-            //    sw.WriteLine("nu,z,terms,y");
-            //    for (double nu = -4d; nu <= 4d; nu += 0.125d) {
-            //        for (double z = 1d; z <= 256d; z += 0.5d) {
-            //            (ddouble y, int terms) = BesselLimit.BesselK(nu, z);
-            //
-            //            if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
-            //                continue;
-            //            }
-            //
-            //            sw.WriteLine($"{nu},{z},{terms},{y}");
-            //            Console.WriteLine($"{nu},{z},{terms},{y}");
-            //
-            //            break;
-            //        }
-            //    }
-            //}
+            using (StreamWriter sw = new StreamWriter("../../bessel_j_limit.csv")) {
+                sw.WriteLine("nu,z,terms,y");
+                for (double nu = -4d; nu <= 4d; nu += 0.125d) {
+                    for (double z = 1d; z <= 256d; z += 0.5d) {
+                        (ddouble y, int terms) = BesselLimit.BesselJ(nu, z);
+            
+                        if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
+                            continue;
+                        }
+            
+                        sw.WriteLine($"{nu},{z},{terms},{y}");
+                        Console.WriteLine($"{nu},{z},{terms},{y}");
+            
+                        break;
+                    }
+                }
+            }
+            
+            using (StreamWriter sw = new StreamWriter("../../bessel_y_limit.csv")) {
+                sw.WriteLine("nu,z,terms,y");
+                for (double nu = -4d; nu <= 4d; nu += 0.125d) {
+                    for (double z = 1d; z <= 256d; z += 0.5d) {
+                        (ddouble y, int terms) = BesselLimit.BesselY(nu, z);
+            
+                        if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
+                            continue;
+                        }
+            
+                        sw.WriteLine($"{nu},{z},{terms},{y}");
+                        Console.WriteLine($"{nu},{z},{terms},{y}");
+            
+                        break;
+                    }
+                }
+            }
+            
+            using (StreamWriter sw = new StreamWriter("../../bessel_i_limit.csv")) {
+                sw.WriteLine("nu,z,terms,y");
+                for (double nu = -4d; nu <= 4d; nu += 0.125d) {
+                    for (double z = 1d; z <= 256d; z += 0.5d) {
+                        (ddouble y, int terms) = BesselLimit.BesselI(nu, z);
+            
+                        if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
+                            continue;
+                        }
+            
+                        sw.WriteLine($"{nu},{z},{terms},{y}");
+                        Console.WriteLine($"{nu},{z},{terms},{y}");
+            
+                        break;
+                    }
+                }
+            }
+            
+            using (StreamWriter sw = new StreamWriter("../../bessel_k_limit.csv")) {
+                sw.WriteLine("nu,z,terms,y");
+                for (double nu = -4d; nu <= 4d; nu += 0.125d) {
+                    for (double z = 1d; z <= 256d; z += 0.5d) {
+                        (ddouble y, int terms) = BesselLimit.BesselK(nu, z);
+            
+                        if (ddouble.IsNaN(y) || !ddouble.IsFinite(y)) {
+                            continue;
+                        }
+            
+                        sw.WriteLine($"{nu},{z},{terms},{y}");
+                        Console.WriteLine($"{nu},{z},{terms},{y}");
+            
+                        break;
+                    }
+                }
+            }
 
             Console.WriteLine("END");
             Console.Read();
