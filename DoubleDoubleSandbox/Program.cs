7021a6ef8b80e79593b55d00d4496e907c53e893
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-22 16:30:48 +0900

    cleanup

---
+++ DoubleDoubleSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleDoubleSandbox {
+    public static class Program {
+        static void Main() { 
+        }
+    }
+}
