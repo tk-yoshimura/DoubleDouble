c112b36fe34a0aba401c6ce34441d9cbf45f7e3d
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-04 18:58:07 +0900

    sandbox incompgamma

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -5,38 +5,39 @@ using System.IO;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            for (double x = -10; x <= 10; x += 1d / 256) {
-                ddouble y = ddouble.AiryBi(x);
+            //using (StreamWriter sw = new("../../incompgamma.csv")) {
+            //    sw.WriteLine("lower");
+            //    sw.WriteLine("s,x,m,y");
+            //
+            //    for (ddouble s = 1d / 32; s <= 1; s += 1d / 32) {
+            //        for (ddouble x = 0; x <= 32; x += 1d / 16) {
+            //            (ddouble y, int m) = IncompleteGammaPrototype.LowerIncompleteGammaConvergence(s, x);
+            //
+            //            Console.WriteLine($"{s}\t{x}\t{m}\t{y}");
+            //            sw.WriteLine($"{s},{x},{m},{y}");
+            //        }
+            //    }
+            //
+            //    sw.WriteLine("upper");
+            //    sw.WriteLine("s,x,m,y");
+            //
+            //    for (ddouble s = 1d / 32; s <= 1; s += 1d / 32) {
+            //        for (ddouble x = 0; x <= 32; x += 1d / 16) {
+            //            (ddouble y, int m) = IncompleteGammaPrototype.UpperIncompleteGammaConvergence(s, x);
+            //
+            //            Console.WriteLine($"{s}\t{x}\t{m}\t{y}");
+            //            sw.WriteLine($"{s},{x},{m},{y}");
+            //        }
+            //    }
+            //}
 
-                Console.WriteLine($"{x},{y}");
-            }
+            IncompleteGammaPrototype.UpperIncompleteGammaNearZero(0.5, 0.5);
 
-
-            using (StreamWriter sw = new("../../incompgamma.csv")) {
-                sw.WriteLine("lower");
-                sw.WriteLine("s,x,m,y");
-
-                for (ddouble s = 1d / 32; s <= 1; s += 1d / 32) {
-                    for (ddouble x = 0; x <= 32; x += 1d / 16) {
-                        (ddouble y, int m) = IncompleteGammaPrototype.LowerIncompleteGammaConvergence(s, x);
-
-                        Console.WriteLine($"{s}\t{x}\t{m}\t{y}");
-                        sw.WriteLine($"{s},{x},{m},{y}");
-                    }
-                }
-
-                sw.WriteLine("upper");
-                sw.WriteLine("s,x,m,y");
-
-                for (ddouble s = 1d / 32; s <= 1; s += 1d / 32) {
-                    for (ddouble x = 0; x <= 32; x += 1d / 16) {
-                        (ddouble y, int m) = IncompleteGammaPrototype.UpperIncompleteGammaConvergence(s, x);
-
-                        Console.WriteLine($"{s}\t{x}\t{m}\t{y}");
-                        sw.WriteLine($"{s},{x},{m},{y}");
-                    }
-                }
-            }
+           //for (ddouble nu = 0; nu <= 1; nu += 1d / 256) {
+           //    ddouble y = IncompleteGammaPrototype.UpperIncompleteGammaNearZero(nu, 0.5);
+           //
+           //    Console.WriteLine($"{nu},{y}");
+           //}
 
             Console.WriteLine("END");
             Console.Read();
