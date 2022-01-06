8d663379be3b621812cf73a13ba22adbfc6f790e
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-06 17:43:22 +0900

    + carlson rf

--- DoubleDoubleSandbox/DDouble_incompelliptic.cs
+++ DoubleDoubleSandbox/DDouble_incompelliptic.cs
@@ -11,6 +11,9 @@ namespace DoubleDoubleSandbox {
             public static ddouble Rcp5 = Rcp(5d);
             public static ddouble Rcp6 = Rcp(6d);
             public static ddouble Rcp7 = Rcp(7d);
+            public static ddouble Rcp10 = Rcp(10d);
+            public static ddouble Rcp14 = Rcp(14d);
+            public static ddouble Rcp24 = Rcp(24d);
 
             public static ddouble C3d8 = (ddouble)3 / 8;
             public static ddouble C3d10 = (ddouble)3 / 10;
@@ -18,6 +21,7 @@ namespace DoubleDoubleSandbox {
             public static ddouble C3d14 = (ddouble)3 / 14;
             public static ddouble C3d22 = (ddouble)3 / 22;
             public static ddouble C3d26 = (ddouble)3 / 26;
+            public static ddouble C3d44 = (ddouble)3 / 44;
             public static ddouble C9d22 = (ddouble)9 / 22;
             public static ddouble C9d52 = (ddouble)9 / 52;
             public static ddouble C9d88 = (ddouble)9 / 88;
@@ -112,6 +116,53 @@ namespace DoubleDoubleSandbox {
                 ddouble v2 = zd * (xy3mzz8 * Rcp6 + zd * (zd * xy * C3d26 - (xy - zz) * C9d22));
 
                 ddouble v = 3d * s + exp4 * (1d + v1 + v2) * (invmu * Sqrt(invmu));
+
+                return v;
+            }
+
+            public static ddouble Rf(ddouble x, ddouble y, ddouble z) {
+                const int max_iters = 64;
+
+                ddouble mu = 0, invmu = 0, xd = 0, yd = 0, zd = 0;
+
+                for (int i = 0; i <= max_iters; i++) {
+                    (ddouble sqrtx, ddouble sqrty, ddouble sqrtz) = (
+                        Sqrt(x), Sqrt(y), Sqrt(z)
+                    );
+
+                    ddouble lambda = (sqrtx + sqrtz) * sqrty + sqrtx * sqrtz;
+
+                    (x, y, z) = (
+                        (x + lambda) / 4,
+                        (y + lambda) / 4,
+                        (z + lambda) / 4
+                    );
+
+                    if ((i & 1) == 0) {
+                        mu = (x + y + z) * Rcp3;
+                        invmu = Rcp(mu);
+
+                        (xd, yd, zd) = (
+                            2d - (mu + x) * invmu,
+                            2d - (mu + y) * invmu,
+                            2d - (mu + z) * invmu
+                        );
+
+                        ddouble eps = Max(Max(Abs(xd), Abs(yd)), Abs(zd));
+
+                        if (!(eps >= 1e-5)) {
+                            break;
+                        }
+                    }
+
+                    if (i >= max_iters) {
+                        throw new ArithmeticException("Carlson integral Rf not convergence.");
+                    }
+                }
+
+                ddouble xymzz = xd * yd - zd * zd, xyz = xd * yd * zd;
+
+                ddouble v = (1d + xyz * Rcp14 - xymzz * (Rcp10 - xymzz * Rcp24 + xyz * C3d44)) * Sqrt(invmu);
 
                 return v;
             }
