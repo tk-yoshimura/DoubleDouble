03c5ed17a704fa22884f95f2a11fcb97f4a7b3f7
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-09 02:20:24 +0900

    fix gamma

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -4,10 +4,24 @@ using System;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            for (double x = 0; x < 24; x += 1d / 32) {
-                ddouble y = ddouble.JacobiAm(x, 0.25);
+            ddouble f = 1;
 
+            for (double x = 0; x <= 36; x += 0.5d) {
+                ddouble y_dec = ddouble.Gamma(ddouble.BitDecrement(x));
+                ddouble y = ddouble.Gamma(x);
+                ddouble y_inc = ddouble.Gamma(ddouble.BitIncrement(x));
+
+                Console.WriteLine($"{x},{y_inc}");
                 Console.WriteLine($"{x},{y}");
+                Console.WriteLine($"{x},{y_dec}");
+
+                //Console.WriteLine($"{FloatSplitter.Split(y).mantissa:X14}");
+
+                //Console.WriteLine($"{x}");
+                //Console.WriteLine($"{(FloatSplitter.Split(y).mantissa):X14}");
+                //Console.WriteLine($"{(FloatSplitter.Split(f).mantissa):X14}");
+                //
+                //f *= x + 1;
             }
 
             Console.WriteLine("END");
