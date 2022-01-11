5eb298a66458a022fbc0e8c4e0bdbcc090465b61
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-11 16:26:20 +0900

    + sandbox incompbeta

--- DoubleDoubleSandbox/DDouble_incompbeta.cs
+++ DoubleDoubleSandbox/DDouble_incompbeta.cs
@@ -5,17 +5,20 @@ namespace DoubleDoubleSandbox {
     public static class IncompBetaPrototype {
 
         public static ddouble Beta(ddouble x, ddouble a, ddouble b, int m) { 
-            ddouble c = 1d / (a + b + 2d);
+            ddouble c = a + b + 2d;
 
-            ddouble ra = (a + 1d) * c, rb = (b + 1d) * c;
-            ddouble t0 = x / ra, t1 = (1d - x) / rb;
+            ddouble t0 = x / (a + 1d) * c, t1 = (1d - x) / (b + 1d) * c;
 
             if (t0 < t1) {
-                return Cfrac(x, a, b, m);
+                return Cfrac2(x, a, b, m);
             }
             else {
-                return ddouble.Beta(a, b) - Cfrac(1d - x, b, a, m);
+                return ddouble.Beta(a, b) - Cfrac2(1d - x, b, a, m);
             }
+        }
+
+        public static ddouble CBeta(ddouble x, ddouble a, ddouble b, int m) { 
+            return Cfrac2(1d - x, b, a, m);
         }
 
         public static ddouble Cfrac(ddouble x, ddouble a, ddouble b, int m) {
@@ -37,11 +40,37 @@ namespace DoubleDoubleSandbox {
             return y;
         }
 
+        public static ddouble Cfrac2(ddouble x, ddouble a, ddouble b, int m) {
+            (ddouble s2, ddouble s1, ddouble s0) = (1, 1, 0);
+            (ddouble t2, ddouble t1, ddouble t0) = (1, 0, 0);
+            
+            for (int n = 0; n <= m; n++) {
+                ddouble na = n + a, nb = n - b, nab = n + a + b;
+                ddouble n2a = 2 * n + a, n2a1 = n2a + 1d, n2a2 = n2a + 2d;
+
+                ddouble p0 = (na * nab * x) / (n2a * n2a1), p1 = ((n + 1d) * (nb + 1d) * x) / (n2a1 * n2a2);
+
+                (s2, s1, s0) = (s2 - p0 * s1, s2, s1);
+                (t2, t1, t0) = (t2 - p0 * t1, t2, t1);
+                (s2, s1, s0) = (s2 - p1 * s1, s2, s1);
+                (t2, t1, t0) = (t2 - p1 * t1, t2, t1);
+
+                ddouble c = 1d / t2;
+                
+                (s2, s1, s0) = (s2 * c, s1 * c, s0 * c);
+                (t2, t1, t0) = (1d, t1 * c, t0 * c);
+            }
+
+            ddouble y = Pow(x, a) * Pow(1d - x, b) / (a * (1d + s2));
+
+            return y;
+        }
+
         public static (ddouble y, int m) BetaConvergence(ddouble x, ddouble a, ddouble b, int max_m = 1024, int convchecks = 4) {
-            ddouble prev_y = Beta(x, a, b, 0);
+            ddouble prev_y = CBeta(x, a, b, 0);
             
             for (int m = 1, convtimes = 0; m <= max_m; m++) {
-                ddouble y = Beta(x, a, b, m);
+                ddouble y = CBeta(x, a, b, m);
 
                 if (ddouble.Abs(y / prev_y - 1) < 1e-30) {
                     convtimes++;
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -5,18 +5,56 @@ using System.IO;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            using (StreamWriter sw = new("../../incompbeta2.csv")) {
+            //using (StreamWriter sw = new("../../incompbeta2.csv")) {
+            //    sw.WriteLine("x,a,b,y,m");
+            //
+            //    for (ddouble b = 1d / 64; b <= 64d; b *= 2) {
+            //        for (ddouble a = 1d / 64; a <= 64d; a *= 2) {
+            //            for (ddouble x = 1d / 32; x <= 31d / 32; x += 1d / 32) {
+            //                (ddouble y, int m) = IncompBetaPrototype.BetaConvergence(x, a, b);
+            //
+            //                sw.WriteLine($"{x},{a},{b},{y},{m}");
+            //
+            //                Console.WriteLine($"{x},{a},{b},{y},{m}");
+            //            }
+            //        }
+            //    }
+            //}
+
+            //using (StreamWriter sw = new("../../incompbeta3.csv")) {
+            //    sw.WriteLine("x,a=b,y,m");
+            //
+            //    ddouble x = 0.5d;
+            //
+            //    for (ddouble a = 20; a <= 160d; a += 1d / 64) {
+            //        (ddouble y, int m) = IncompBetaPrototype.BetaConvergence(x, a, a);
+            //
+            //        sw.WriteLine($"{x},{a},{y},{m}");
+            //
+            //        Console.WriteLine($"{x},{a},{y},{m}");
+            //    }
+            //}
+
+            using (StreamWriter sw = new("../../incompbeta6.csv")) {
                 sw.WriteLine("x,a,b,y,m");
+            
+                for (ddouble b = 1d / 16; b <= 32d; b += 1d / 16) {
+                    if (b % 1 == 0) {
+                        continue;
+                    }
 
-                for (ddouble b = 1d / 64; b <= 64d; b *= 2) {
-                    for (ddouble a = 1d / 64; a <= 64d; a *= 2) {
-                        for (ddouble x = 1d / 32; x <= 31d / 32; x += 1d / 32) {
-                            (ddouble y, int m) = IncompBetaPrototype.BetaConvergence(x, a, b);
-
-                            sw.WriteLine($"{x},{a},{b},{y},{m}");
-
-                            Console.WriteLine($"{x},{a},{b},{y},{m}");
+                    for (ddouble a = 1d / 16; a <= 32d; a += 1d / 16) {
+                        if (a % 1 == 0) {
+                            continue;
                         }
+                        
+                        ddouble x = (a + 1) / (a + b + 2);
+
+                        (ddouble y, int m) = IncompBetaPrototype.BetaConvergence(x, a, b);
+            
+                        sw.WriteLine($"{x},{a},{b},{y},{m}");
+            
+                        Console.WriteLine($"{x},{a},{b},{y},{m}");
                     }
                 }
             }
