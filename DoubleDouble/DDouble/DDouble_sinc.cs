e9e22ffbe0384436b606a7e9829452e1331f0e7b
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-12 22:46:16 +0900

    + sinc

--- DoubleDouble/DDouble/DDouble_sinc.cs
+++ DoubleDouble/DDouble/DDouble_sinc.cs
@@ -3,14 +3,28 @@
 namespace DoubleDouble {
     public partial struct ddouble {
         public static ddouble Sinc(ddouble x, bool normalized = true) {
-            if (Abs(x) < Math.ScaleB(1, -105)) {
-                return 1d;
-            }
-
             if (normalized) {
-                return SinPI(x) / (PI * x);
+                if (Abs(x) < Math.ScaleB(1, -64)) {
+                    return 1d - Square(x * PI) / 6d;
+                }
+
+                ddouble c = PI * x;
+
+                if (IsInfinity(c)) {
+                    return Zero;
+                }
+
+                return SinPI(x) / c;
             }
             else {
+                if (Abs(x) < Math.ScaleB(1, -64)) {
+                    return 1d - x * x / 6d;
+                }
+
+                if (IsInfinity(x)) {
+                    return Zero;
+                }
+
                 return Sin(x) / x;
             }
         }
