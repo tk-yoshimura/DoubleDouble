bf314c41d98ea38c006bcd98ab10ec685179ea1a
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-12 13:06:10 +0900

    Update Program.cs

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using DoubleDouble;
 
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
+            ddouble c = "3.8317059702075123156144358863081607665645452742878019287622989899";
+
+            for (ddouble x = c - 1e-29; x < c + 1e-29; x += 1e-30) {
+                ddouble y = ddouble.BesselJ(1, x);
+
+                Console.WriteLine(y);
+            }
+
             Console.WriteLine("END");
             Console.Read();
         }
