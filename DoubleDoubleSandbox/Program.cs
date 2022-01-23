c54634b99ee045cc4169a687e72470ac2041f3a9
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-24 02:41:43 +0900

    sandbox struve

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -4,21 +4,7 @@ using System;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            for (double x = 0; x <= 42; x += 0.0625d) {
-                ddouble y_dec = ddouble.Gamma(ddouble.BitDecrement(x));
-                ddouble y = ddouble.Gamma(x);
-                ddouble y_inc = ddouble.Gamma(ddouble.BitIncrement(x));
-
-                Console.WriteLine($"{x}");
-
-                //Console.WriteLine($"{y_inc}");
-                //Console.WriteLine($"{y}");
-                //Console.WriteLine($"{y_dec}");
-
-                Console.WriteLine($"{FloatSplitter.Split(y_inc).mantissa:X14}");
-                Console.WriteLine($"{FloatSplitter.Split(y).mantissa:X14}");
-                Console.WriteLine($"{FloatSplitter.Split(y_dec).mantissa:X14}");
-            }
+            ddouble y = Struve.StruveKIntegral.Value(3, 2);
 
             Console.WriteLine("END");
             Console.Read();
