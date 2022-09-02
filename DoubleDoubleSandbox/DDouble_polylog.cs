2efb55460c3790f92efee0de8dc56a586b2355cf
tk-yoshimura <yoshimura8192@gmail.com>
2022-09-03 00:07:48 +0900

    sandbox polylog

---
+++ DoubleDoubleSandbox/DDouble_polylog.cs
@@ -0,0 +1,148 @@
+﻿using DoubleDouble;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using static DoubleDouble.ddouble;
+
+namespace DoubleDoubleSandbox {
+    public static class PolylogSanbox {
+        public static ddouble Polylog(int n, ddouble x) {
+            throw new NotImplementedException();
+        }
+
+        public static class PolylogNearOne {
+            public static ddouble Polylog(int n, ddouble x) {
+                if (x < 0.5 || x > 1) {
+                    throw new ArgumentOutOfRangeException(nameof(x));
+                }
+
+                if (1 - x < Math.ScaleB(1, -100)) {
+                    return RiemannZeta(n);
+                }
+
+                ddouble v = Log(x), v2 = v * v;
+
+                ReadOnlyCollection<ddouble> coef = CoefTable.Coef(n);
+
+                ddouble y = Pow(v, n - 1) * TaylorSequence[n - 1] * (HarmonicNumber(n - 1) - Log(-v)); 
+                ddouble u = 1;
+
+                for(int k = 0; k <= n; k++) {
+                    if (k == n - 1) {
+                        u *= v;
+                        continue;
+                    }
+
+                    ddouble dy = coef[k] * u;
+                    ddouble y_next = y + dy;
+
+                    if (y == y_next) {
+                        return y;
+                    }
+
+                    u *= v;
+                    y = y_next;
+                }
+
+                for(int k = n + 1; k < coef.Count - 1; k += 2) {
+                    ddouble dy = coef[k] * u;
+                    ddouble y_next = y + dy;
+
+                    if (y == y_next) {
+                        return y;
+                    }
+
+                    u *= v2;
+                    y = y_next;
+                }
+
+                return NaN;
+            }
+
+            public static class CoefTable {
+                private static readonly Dictionary<int, ReadOnlyCollection<ddouble>> table = new();
+                
+                public static ReadOnlyCollection<ddouble> Coef(int n) {
+                    if (n < 2) {
+                        throw new ArgumentOutOfRangeException(nameof(n));
+                    }
+                    
+                    if (table.ContainsKey(n)) {
+                        return table[n];
+                    }
+                    
+                    List<ddouble> coef = new List<ddouble>();
+                    coef.Add(RiemannZeta(n));
+                    for (int k = 1; k < TaylorSequence.Count; k++) {
+                        coef.Add(RiemannZeta(n - k) * TaylorSequence[k]);
+                    }
+
+                    table.Add(n, new ReadOnlyCollection<ddouble>(coef));
+
+                    return table[n];
+                }
+            }
+        }
+
+
+        public static class PolylogLimit {
+            public static ddouble Polylog(int n, ddouble x) {
+                if (x > -1.5) {
+                    throw new ArgumentOutOfRangeException(nameof(x));
+                }
+
+                x = -x;
+
+                ddouble y = bias[n](Log(x));
+                ddouble u = 1 / x, v = u, v2 = v * v;
+
+                for (int i = 1; i <= 2048; i += 2) {
+                    ddouble dy = u * (Pow(i, -n) - v * Pow(i + 1, -n));
+                    ddouble y_next = ((n & 1) == 0) ? (y - dy) : (y + dy);
+
+                    if (y == y_next) {
+                        return y;
+                    }
+
+                    y = y_next;
+                    u *= v2;
+                }
+
+                return NaN;
+            }
+
+            static ddouble pi2 = Square(PI);
+            static ddouble pi4 = Pow(PI, 4);
+            static ddouble pi6 = Pow(PI, 6);
+            static ddouble pi8 = Pow(PI, 8);
+
+            static ReadOnlyCollection<Func<ddouble, ddouble>> bias = new(new Func<ddouble, ddouble>[] {
+               (logx) => throw new NotImplementedException(),
+               (logx) => throw new NotImplementedException(),
+               (logx) => (pi2 + 3 * Square(logx)) / 6,
+               (logx) => logx * (pi2 + Square(logx)) / 6,
+               (logx) => {
+                   ddouble logx2 = Square(logx);
+                   return (7 * pi4 + logx2 * (30 * pi2 + 15 * logx2)) / 360;
+               },
+               (logx) => {
+                   ddouble logx2 = Square(logx);
+                   return logx * (7 * pi4 + logx2 * (10 * pi2 + 3 * logx2)) / 360;
+               },
+               (logx) => {
+                   ddouble logx2 = Square(logx);
+                   return (31 * pi6 + logx2 * (147 * pi4 + logx2 * (105 * pi2 + 21 * logx2))) / 15120;
+               },
+               (logx) => {
+                   ddouble logx2 = Square(logx);
+                   return logx * (31 * pi6 + logx2 * (49 * pi4 + logx2 * (21 * pi2 + 3 * logx2))) / 15120;
+               },
+               (logx) => {
+                   ddouble logx2 = Square(logx);
+                   return (127 * pi8 + logx2 * (620 * pi6 + logx2 * (490 * pi4 + logx2 * (140 * pi2 + 15 * logx2)))) / 604800;
+               },
+            });
+        }
+    }
+}
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -4,13 +4,60 @@ using DoubleDouble;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            for(ddouble x = -8; x <= 8; x += 1.0 / 256){
-                ddouble y = ddouble.Polygamma(1, x);
-                Console.WriteLine($"{x},{y}");
+            //for (int n = 2; n <= 8; n++) {
+            //    ddouble x = 1.5;
+            //    ddouble y = PolylogSanbox.PolylogLimit.Polylog(n, -x);
+            //
+            //    if (!ddouble.IsNaN(y)) {
+            //        Console.WriteLine($"{n},{x},{y}");
+            //    }
+            //}
+
+            for (int n = 2; n <= 8; n++) {
+                for (ddouble x = 0.5; x > Math.ScaleB(1, -102); x /= 2) {
+                    ddouble y = PolylogSanbox.PolylogNearOne.Polylog(n, 1 - x);
+            
+                    Console.WriteLine($"{n},{1 - x},{y}");
+                }
             }
 
             Console.WriteLine("END");
             Console.Read();
+        }
+
+        static double PolylogPositiveIntegrandPeak(int n, double x) {
+            if (n <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+            if (n == 1) {
+                return 0;
+            }
+
+            double t;
+            if (n == 2) {
+                double u = Math.Sqrt(-Math.Log(x));
+                t = u * (1.38264783 - u * 0.55707189); 
+            }
+            else {
+                t = n - 1;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                double xexp = Math.Exp(-t) * x;
+                double d = (1 - n) * (xexp - 1) - t;
+                double dv = (1 - n) * xexp + 1;
+                double dt = d / dv;
+
+                t += dt;
+
+                Console.WriteLine(dt);
+
+                if (Math.Abs(dt / t) < 1e-14) {
+                    break;
+                }
+            }
+
+            return t;
         }
     }
 }
