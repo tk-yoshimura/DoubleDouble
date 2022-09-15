103bf0aa06d35b46d54307457cd7fdf81a68b041
tk-yoshimura <yoshimura8192@gmail.com>
2022-09-15 15:53:49 +0900

    + polynomials

---
+++ DoubleDouble/DDouble/DDouble_laguerre.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace DoubleDouble {
+    public partial struct ddouble {
+        public ReadOnlyCollection<Func<ddouble, ddouble>> LaguerrePolynomials => Consts.Laguerre.LaguerrePolynomials;
+
+        internal static partial class Consts {
+            public static class Laguerre {
+                public static ReadOnlyCollection<Func<ddouble, ddouble>> LaguerrePolynomials = 
+                    new ReadOnlyCollection<Func<ddouble, ddouble>>(new Func<ddouble, ddouble>[]{
+                        (x) => 1d, 
+                        (x) => 1d - x,
+                        (x) => {
+                            return (2d + x * (-4d + x)) / 2;  
+                        },
+                        (x) => {
+                            return (6d + x * (-18d + x * (9d + x))) / 6;  
+                        },
+                        (x) => {
+                            return (24d + x * (-96d + x * (72d + x * (-16d + x)))) / 24;  
+                        },
+                        (x) => {
+                            return (120d + x * (-600d + x * (600d + x * (-200d + x * (25d + x))))) / 120;  
+                        },
+                        (x) => {
+                            return (720d + x * (-4320d + x * (5400d + x * (-2400d + x * (450d + x * (-36d + x)))))) / 720;  
+                        },
+                        (x) => {
+                            return (5040d + x * (-35280d + x * (52920d + x * (-29400d + x * (7350d + x * (-882d + x * (49d + x))))))) / 5040; 
+                        },
+                        (x) => {
+                            return (40320d + x * (-322560d + x * (564480d + x * (-376320d + x * (117600d + x * (-18816d + x * (1568d + x * (-64d + x)))))))) / 40320; 
+                        },
+                        (x) => {
+                            return (362880d + x * (-3265920d + x * (6531840d + x * (-5080320d + x * (1905120d + x * (-381024d + x * (42336d + x * (-2592d + x * (81d + x))))))))) / 362880; 
+                        },
+                        (x) => {
+                            return (3628800d + x * (-36288000d + x * (81648000d + x * (-72576000d + x * (31752000d + x * (-7620480d + x * (1058400d + x * (-86400d + x * (4050d + x * (-100d + x)))))))))) / 362880;  
+                        }
+                });
+            }
+        }
+    }
+}
---
+++ DoubleDouble/DDouble/DDouble_legendre.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace DoubleDouble {
+    public partial struct ddouble {
+        public ReadOnlyCollection<Func<ddouble, ddouble>> LegendrePolynomials => Consts.Legendre.LegendrePolynomials;
+
+        internal static partial class Consts {
+            public static class Legendre {
+                public static ReadOnlyCollection<Func<ddouble, ddouble>> LegendrePolynomials = 
+                    new ReadOnlyCollection<Func<ddouble, ddouble>>(new Func<ddouble, ddouble>[]{
+                        (x) => 1d, 
+                        (x) => x,
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-1d + x2 * 3d, -1);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp((-6d + x2 * 10d) * x, -2);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(3d + x2 * (-30d + x2 * 35d), -3);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp((30d + x2 * (-140d + x2 * 126d)) * x, -4);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-10d + x2 * (210d + x2 * (-630d + x2 * 462d)), -5);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-140d + x2 * (1260d + x2 * (-2772d + x2 * 1716d)) * x, -6);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(35d + x2 * (-1260d + x2 * (6930d + x2 * (-12012d + x2 * 6435d))), -7);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(630d + x2 * (-9240d + x2 * (36036d + x2 * (-51480d + x2 * 24310d))) * x, -8);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-126d + x2 * (6930d + x2 * (-60060d + x2 * (180180d + x2 * (-218790d + x2 * 92378d)))), -9);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-2772d + x2 * (60060d + x2 * (-360360d + x2 * (875160d + x2 * (-923780d + x2 * 352716d)))) * x, -10);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(462d + x2 * (-36036d + x2 * (450450d + x2 * (-2042040d + x2 * (4157010d + x2 * (-3879876d + x2 * 1352078d))))), -11);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(12012d + x2 * (-360360d + x2 * (3063060d + x2 * (-11085360d + x2 * (19399380d + x2 * (-16224936d + x2 * 5200300d))))) * x, -12);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-1716d + x2 * (180180d + x2 * (-3063060d + x2 * (19399380d + x2 * (-58198140d + x2 * (89237148d + x2 * (-67603900d + x2 * 20058300d)))))), -13);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(-51480d + x2 * (2042040d + x2 * (-23279256d + x2 * (116396280d + x2 * (-297457160d + x2 * (405623400d + x2 * (-280816200d + x2 * 77558760d)))))) * x, -14);  
+                        },
+                        (x) => {
+                            ddouble x2 = x * x;
+                            return Ldexp(6435d + x2 * (-875160d + x2 * (19399380d + x2 * (-162954792d + x2 * (669278610d + x2 * (-1487285800d + x2 * (1825305300d + x2 * (-1163381400d + x2 * 300540195d))))))), -15);  
+                        },
+                });
+            }
+        }
+    }
+}
