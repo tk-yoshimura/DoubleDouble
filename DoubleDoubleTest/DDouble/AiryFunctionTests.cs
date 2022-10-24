b83568b59ad83c877eb4b0373656f15a3de2bd25
tk-yoshimura <yoshimura8192@gmail.com>
2022-10-25 04:38:17 +0900

    fix airy test

--- DoubleDoubleTest/DDouble/AiryFunctionTests.cs
+++ DoubleDoubleTest/DDouble/AiryFunctionTests.cs
@@ -537,9 +537,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 8e-29d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 8e-29d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 8e-29d);
             }
 
             for ((int i, ddouble x) = (0, -1); i < nz_expecteds.Length; i++, x += 1d / 128) {
@@ -552,9 +552,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 8e-29d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 8e-29d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 8e-29d);
             }
 
             Assert.IsTrue(ddouble.IsNaN(ddouble.AiryAi(ddouble.NaN)));
@@ -1098,9 +1098,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-29d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-29d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-29d);
             }
 
             for ((int i, ddouble x) = (0, -1); i < nz_expecteds.Length; i++, x += 1d / 128) {
@@ -1113,9 +1113,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-29d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-29d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-29d);
             }
 
             Assert.IsTrue(ddouble.IsNaN(ddouble.AiryBi(ddouble.NaN)));
