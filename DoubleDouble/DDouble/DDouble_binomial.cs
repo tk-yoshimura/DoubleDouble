84ae57ffc8ad97d121fc53d1602383a7e057c189
tk-yoshimura <yoshimura8192@gmail.com>
2022-10-26 00:46:50 +0900

    + binomial

---
+++ DoubleDouble/DDouble/DDouble_binomial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DoubleDouble {
+    public partial struct ddouble {
+        public static ddouble Binomial(int n, int k) {
+            if (k > n / 2) {
+                return Binomial(n, checked(n - k));
+            }
+
+            if (n < 0 || k > n || k < 0) {
+                throw new ArgumentOutOfRangeException($"{nameof(n)},{nameof(k)}");
+            }
+
+            if (n > 1000) { 
+                throw new ArgumentOutOfRangeException(
+                    nameof(n),
+                    "In the calculation of the Binomial function, n greater than 1000 is not supported."
+                );
+            }
+
+            if (k == 0) {
+                return 1d;
+            }
+            if (k == 1) {
+                return n;
+            }
+
+            ddouble v = n;
+            for (int i = 2; i <= k; i++) {
+                v *= (double)(n - i + 1);
+                v /= i;
+            }
+
+            v = Round(v);
+
+            return v;
+        }
+    }
+}
---
+++ DoubleDoubleTest/DDouble/BinomialTests.cs
@@ -0,0 +1,36 @@
+﻿using DoubleDouble;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DoubleDoubleTest.DDouble {
+    [TestClass]
+    public class BinomialTests {
+        [TestMethod]
+        public void BinomialTest() {
+            for (int n = 0; n <= 1000; n++) {
+                for (int k = 0; k <= n; k += n / 32 + 1) {
+                    Console.WriteLine($"checked {n},{k}");
+
+                    if (k == 0 || k == n) {
+                        Assert.AreEqual(1, ddouble.Binomial(n, k), $"{n},{k}");
+                    }
+                    else {
+                        ddouble expected = ddouble.Binomial(n - 1, k - 1) + ddouble.Binomial(n - 1, k);
+                        ddouble actual = ddouble.Binomial(n, k);
+
+                        if (expected < 1e+29) {
+                            Assert.AreEqual(expected, actual, $"{n},{k}");
+                        }
+                        else {
+                            HPAssert.AreEqual(expected, actual, expected * 8e-31, $"{n},{k}");
+                        }
+                    }
+                }
+            }
+
+            Assert.IsTrue(ddouble.IsFinite(ddouble.Binomial(999, 499)));
+            Assert.IsTrue(ddouble.IsFinite(ddouble.Binomial(999, 500)));
+            Assert.IsTrue(ddouble.IsFinite(ddouble.Binomial(1000, 500)));
+        }
+    }
+}
