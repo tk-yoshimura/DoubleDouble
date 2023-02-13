aa228df79a08ccc1a4f4516513848165f44dc63b
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-13 23:32:31 +0900

    fix sandbox

--- DoubleDouble/Properties/AssemblyInfo.cs
+++ DoubleDouble/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DoubleDouble")]
-[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2021-2022")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2021-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -6,13 +6,11 @@ namespace DoubleDoubleSandbox {
         static void Main() {
             const int n = 4;
 
-            for (ddouble u = 0; u <= 8192; u += 16) {
-                ddouble q = ddouble.Sqrt(u) * n * n;
-
+            for (ddouble q = 0; q <= 16; q += 1d / 256) {
                 ddouble a = ddouble.MathieuA(n, q);
                 ddouble b = ddouble.MathieuB(n, q);
 
-                Console.WriteLine($"{u},{a},{b}");
+                Console.WriteLine($"{q},{a},{b}");
             }
 
             //for (int exp = -48; exp <= 0; exp++) {
