77c5916f7954acd034202e189066608eaaa689aa
tk-yoshimura <yoshimura8192@gmail.com>
2023-03-12 02:26:03 +0900

    update sandbox

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using DoubleDouble;
 
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
+            for (ddouble x = 0; x <= 32; x += 1d / 32) {
+                ddouble y = ddouble.BesselY(-15.5, x);
+
+                Console.WriteLine($"{x},{y}");
+            }
+
             Console.WriteLine("END");
             Console.Read();
         }
