205ee2af2ba787546c76a60b275871ed34b7b09b
tk-yoshimura <yoshimura8192@gmail.com>
2023-09-05 22:16:46 +0900

    fix inumber

--- DoubleDouble/DDouble/DDouble_inumber.cs
+++ DoubleDouble/DDouble/DDouble_inumber.cs
@@ -38,7 +38,7 @@ namespace DoubleDouble {
         public static ddouble NegativeOne => MinusOne;
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        static int INumberBase<ddouble>.Radix => 10;
+        static int INumberBase<ddouble>.Radix => 2;
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         static ddouble IFloatingPointConstants<ddouble>.E => ddouble.E;
@@ -47,9 +47,9 @@ namespace DoubleDouble {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         static ddouble IFloatingPointConstants<ddouble>.Tau => ddouble.PI * 2;
 
-        public static bool IsCanonical(ddouble value) => true;
+        public static bool IsCanonical(ddouble value) => IsFinite(value);
 
-        public static bool IsRealNumber(ddouble value) => true;
+        public static bool IsRealNumber(ddouble value) => !IsNaN(value);
         public static bool IsImaginaryNumber(ddouble value) => false;
         public static bool IsComplexNumber(ddouble value) => false;
 
@@ -57,8 +57,8 @@ namespace DoubleDouble {
         public static bool IsNegative(ddouble value) => double.IsNegative(value.Hi);
 
         public static bool IsInteger(ddouble value) => Truncate(value) == value;
-        public static bool IsEvenInteger(ddouble value) => value % 2d == 0d;
-        public static bool IsOddInteger(ddouble value) => value % 2d == 1d;
+        public static bool IsEvenInteger(ddouble value) => IsInteger(value) && (Abs(value % 2d) == 0d);
+        public static bool IsOddInteger(ddouble value) => IsInteger(value) && (Abs(value % 2d) == 1d);
 
         public static ddouble MaxMagnitude(ddouble x, ddouble y) => (Abs(x) > Abs(y) || IsNaN(x)) ? x : y;
         public static ddouble MaxMagnitudeNumber(ddouble x, ddouble y) => (Abs(x) > Abs(y) || IsNaN(y)) ? x : y;
@@ -91,6 +91,10 @@ namespace DoubleDouble {
                 result = vi;
                 return true;
             }
+            if (value is decimal vdec) {
+                result = vdec;
+                return true;
+            }
 
             result = default;
             return false;
@@ -105,40 +109,54 @@ namespace DoubleDouble {
         }
 
         public static bool TryConvertToChecked<TOther>(ddouble value, out TOther result) where TOther : INumberBase<TOther> {
-            result = default;
-
-            if (result is double) {
+            if (typeof(TOther) == typeof(double)) {
                 result = (TOther)(object)(double)value;
+                return true;
             }
-            if (result is float) {
+            if (typeof(TOther) == typeof(float)) {
                 result = (TOther)(object)(float)value;
+                return true;
             }
-            if (result is long) {
+            if (typeof(TOther) == typeof(long)) {
                 result = (TOther)(object)(long)value;
+                return true;
             }
-            if (result is int) {
+            if (typeof(TOther) == typeof(int)) {
                 result = (TOther)(object)(int)value;
+                return true;
+            }
+            if (typeof(TOther) == typeof(decimal)) {
+                result = (TOther)(object)(decimal)value;
+                return true;
             }
 
+            result = default;
             return false;
         }
 
         public static bool TryConvertToSaturating<TOther>(ddouble value, out TOther result) where TOther : INumberBase<TOther> {
-            result = default;
-
-            if (result is double) {
+            if (typeof(TOther) == typeof(double)) {
                 result = (TOther)(object)(double)value;
+                return true;
             }
-            if (result is float) {
+            if (typeof(TOther) == typeof(float)) {
                 result = (TOther)(object)(float)ddouble.Clamp(value, float.MinValue, float.MaxValue);
+                return true;
             }
-            if (result is long) {
+            if (typeof(TOther) == typeof(long)) {
                 result = (TOther)(object)(long)ddouble.Clamp(value, long.MinValue, long.MaxValue);
+                return true;
             }
-            if (result is int) {
+            if (typeof(TOther) == typeof(int)) {
                 result = (TOther)(object)(int)ddouble.Clamp(value, int.MinValue, int.MaxValue);
+                return true;
+            }
+            if (typeof(TOther) == typeof(decimal)) {
+                result = (TOther)(object)(decimal)ddouble.Clamp(value, decimal.MinValue, decimal.MaxValue);
+                return true;
             }
 
+            result = default;
             return false;
         }
 
@@ -160,7 +178,7 @@ namespace DoubleDouble {
         }
 
         public bool Equals(ddouble x, ddouble y) => x == y;
-    
+
         public int GetHashCode([DisallowNull] ddouble obj) => obj.GetHashCode();
     }
 }
