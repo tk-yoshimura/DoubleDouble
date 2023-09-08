cf90d2572f57fa2ef664e626bb1212de167f0ff4
tk-yoshimura <yoshimura8192@gmail.com>
2023-09-08 22:26:00 +0900

    fix test tostr

--- DoubleDoubleTest/DDouble/StringTests.cs
+++ DoubleDoubleTest/DDouble/StringTests.cs
@@ -330,6 +330,36 @@ namespace DoubleDoubleTest.DDouble {
                 HPAssert.AreEqual(x, y, x * 1e-30);
                 Assert.AreEqual(exp, double.ILogB(y.Hi));
             }
+
+            for ((ddouble x, int exp) = (ddouble.Ldexp(1, -868) + ddouble.Ldexp(1, -964) * ddouble.Rcp(7), -868); x > 0; x /= 2, exp--) {
+                ddouble y = x.ToString();
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+
+                HPAssert.AreEqual(x, y, x * 1e-30);
+                Assert.AreEqual(exp, double.ILogB(y.Hi));
+            }
+
+            for ((ddouble x, int exp) = (ddouble.Ldexp(1, -868) + ddouble.Ldexp(1, -932) * ddouble.Rcp(7), -868); x > 0; x /= 2, exp--) {
+                ddouble y = x.ToString();
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+
+                HPAssert.AreEqual(x, y, x * 1e-30);
+                Assert.AreEqual(exp, double.ILogB(y.Hi));
+            }
+
+            for ((ddouble x, int exp) = (ddouble.Ldexp(1, -868) + ddouble.Ldexp(1, -900) * ddouble.Rcp(7), -868); x > 0; x /= 2, exp--) {
+                ddouble y = x.ToString();
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+
+                HPAssert.AreEqual(x, y, x * 1e-30);
+                Assert.AreEqual(exp, double.ILogB(y.Hi));
+            }
         }
     }
 }
