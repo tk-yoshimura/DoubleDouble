e2f13fcfbdac3f0f4c874949650e09306117d199
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-02 23:21:52 +0900

    add inverse incomplete gamma

---
+++ DoubleDouble/DDouble/DDouble_invincompgamma.cs
@@ -0,0 +1,52 @@
+﻿namespace DoubleDouble {
+    public partial struct ddouble {
+        public static ddouble InverseLowerIncompleteGamma(ddouble nu, ddouble x) {
+            if (!(x >= 0d && x <= 1d)) {
+                return NaN;
+            }
+
+            if (x == 1d) {
+                return PositiveInfinity;
+            }
+
+            if (x > 0.5d) {
+                return InverseUpperIncompleteGamma(nu, 1d - x);
+            }
+
+            ddouble u = Log2(x), lngamma = LogGamma(nu);
+            ddouble v = nu;
+
+            for (int i = 0; i < 32; i++) {
+                ddouble lnv = Log2(v);
+                ddouble f = LowerIncompleteGammaCFrac.Value(nu, v);
+                ddouble y = nu * lnv - (v + lngamma) * LbE - Log2(f);
+                ddouble g = Exp(-v) * Pow(v, nu - 1d) / LowerIncompleteGamma(nu, v) * LbE;
+                ddouble dv = (y - u) / g;
+
+                ddouble c = Pow2(y);
+                ddouble p = LowerIncompleteGammaRegularized(nu, v);
+
+                Console.WriteLine($"{v},{y},{g}");
+
+                v = Max(0d, v - dv);
+            }
+
+            return v;
+        }
+
+        public static ddouble InverseUpperIncompleteGamma(ddouble nu, ddouble x) {
+            if (!(x >= 0d && x <= 1d)) {
+                return NaN;
+            }
+
+            if (x > 0.5d) {
+                return InverseLowerIncompleteGamma(nu, 1d - x);
+            }
+
+            ddouble u = Log2(x), lngamma = LogGamma(nu);
+            ddouble v0 = nu;
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -4,10 +4,12 @@ using System;
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            for (ddouble x = 0; x <= 32; x += 1d / 4) {
-                ddouble y = ddouble.Erfc(x);
+            ddouble.InverseLowerIncompleteGamma(64, 0.25);
 
-                Console.WriteLine($"{x},{y}");
+            for (ddouble x = 0; x <= 1d / (4096 * 4096); x += 1d / (4096 * 4096 * 4096L)) {
+                ddouble t1 = ddouble.LowerIncompleteGammaRegularized(4, x);
+                ddouble t2 = ddouble.UpperIncompleteGammaRegularized(4, x);
+                Console.WriteLine($"{x},{t1},{t2}");
             }
 
             Console.WriteLine("END");
