93a17d8362294797391a1eb3c5ee79084eb27d4c
tk-yoshimura <yoshimura8192@gmail.com>
2023-10-07 03:01:15 +0900

    fix inverse incomplete gamma p5 approx

--- DoubleDoubleSandbox/Program.cs
+++ DoubleDoubleSandbox/Program.cs
@@ -1,24 +1,63 @@
 ﻿using DoubleDouble;
 using System;
+using System.IO;
 
 namespace DoubleDoubleSandbox {
     public static class Program {
         static void Main() {
-            //ddouble.InverseLowerIncompleteGamma(4, "1e-2");
-            ddouble.InverseUpperIncompleteGamma("0.125", "1e-8");
+            StreamWriter sw = new("../../lower_incomp_gamma_p5_approx.csv");
 
-            ddouble nu = "0.001";
+            sw.WriteLine("nu,x,y");
 
-            for (ddouble x = 0; x <= "1e-20"; x += "1e-23") {
-                ddouble t1 = ddouble.LowerIncompleteGammaRegularized(nu, x);
-                ddouble t2 = ddouble.Pow(x, nu) / (nu * ddouble.Gamma(nu));
-                //ddouble v = ddouble.Pow(t2 * nu * ddouble.Gamma(nu), 1d / nu);
-                //ddouble u = ddouble.Exp((ddouble.Log(t2) + ddouble.Log(nu) + ddouble.LogGamma(nu)) / nu);
-                Console.WriteLine($"{x},{t1},{t2}");
+            for (int exp = -32; exp <= 14; exp++) {
+                for (ddouble v = 1; v < 2; v += 1d / 256) {
+                    ddouble nu = ddouble.Ldexp(v, exp);
+
+                    if (nu > 8192) {
+                        continue;
+                    }
+
+                    ddouble x = ddouble.InverseLowerIncompleteGamma(nu, 0.5);
+                    ddouble y = ddouble.LowerIncompleteGammaRegularized(nu, x);
+
+                    ddouble x_approx = IncompleteGammaP5(nu.Hi);
+                    ddouble y_approx = ddouble.LowerIncompleteGammaRegularized(nu, x_approx);
+
+
+                    if (x < ddouble.Epsilon) { 
+                        continue;
+                    }
+
+                    Console.WriteLine($"{nu},{x},{y},{x_approx},{y_approx}");
+                    sw.WriteLine($"{nu},{x},{y},{x_approx},{y_approx}");
+                }
             }
+
+            sw.Close();
 
             Console.WriteLine("END");
             Console.Read();
+        }
+
+        static double IncompleteGammaP5(double nu) {
+            double nu_ln2 = double.Log2(nu);
+
+            if (nu <= 1) {
+                double b = nu * (1.184797 + nu * (-1.64793 + nu * (-1.87704 + nu * (3.354884 + nu * (-1.93304)))));
+                double c = b - nu_ln2;
+                double x = double.Exp2(-double.Exp2(c));
+
+                return x;
+            }
+            else {
+                double b = 
+                    (-0.92055 + nu_ln2 * (-1.34724 + nu_ln2 * (-0.41283 + nu_ln2 * (-0.11429))))
+                    / (1 + nu_ln2 * (0.295360 + nu_ln2 * (0.114197)));
+
+                double x = nu * double.Exp2(-double.Exp2(b));
+
+                return x;
+            }
         }
     }
 }
