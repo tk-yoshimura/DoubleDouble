89e39eef150212aaac2253c26ab425eeb92ac583
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-10 23:35:56 +0900

    add: logit, expit

---
+++ DoubleDouble/DDouble/DDouble_logit.cs
@@ -0,0 +1,23 @@
+﻿namespace DoubleDouble {
+    public partial struct ddouble {
+        public static ddouble Logit(ddouble x) {
+            if (x < 0.375d || x > 0.625d) {
+                ddouble y = Log(x / (1d - x));
+
+                return y;
+            }
+            else {
+                ddouble s = 2d * (x - 0.5d);
+                ddouble y = Log1p(s) - Log1p(-s);
+
+                return y;
+            }
+        }
+
+        public static ddouble Expit(ddouble x) {
+            ddouble y = 1d / (1d + Exp(-x));
+
+            return y;
+        }
+    }
+}
--- DoubleDoubleTest/DDouble/LogFunctionTests.cs
+++ DoubleDoubleTest/DDouble/LogFunctionTests.cs
@@ -252,15 +252,137 @@ namespace DoubleDoubleTest.DDouble {
 
             ddouble log_pzero = ddouble.Log1p(0d);
             ddouble log_mzero = ddouble.Log1p(-0d);
-            ddouble log_pinf = ddouble.Log(double.PositiveInfinity);
-            ddouble log_ninf = ddouble.Log(double.NegativeInfinity);
-            ddouble log_nan = ddouble.Log(double.NaN);
+            ddouble log_pinf = ddouble.Log1p(double.PositiveInfinity);
+            ddouble log_ninf = ddouble.Log1p(double.NegativeInfinity);
+            ddouble log_nan = ddouble.Log1p(double.NaN);
 
             Assert.IsTrue(ddouble.IsPlusZero(log_pzero), nameof(log_pzero));
             Assert.IsTrue(ddouble.IsMinusZero(log_mzero), nameof(log_mzero));
             Assert.IsTrue(ddouble.IsPositiveInfinity(log_pinf), nameof(log_pinf));
             Assert.IsTrue(ddouble.IsNaN(log_ninf), nameof(log_ninf));
             Assert.IsTrue(ddouble.IsNaN(log_nan), nameof(log_nan));
+        }
+
+        [TestMethod]
+        public void LogitTest() {
+            ddouble[] expecteds = {
+                "-3.43398720448514624592916432454235721044994e0",
+                "-2.70805020110221006599600457014871334417309e0",
+                "-2.26868354131836433578802679543938590084702e0",
+                "-1.94591014905531330510535274344317972963709e0",
+                "-1.68639895357022869958497637754138947441687e0",
+                "-1.46633706879342704465824220464279290015778e0",
+                "-1.27296567581288744409616592300919554941412e0",
+                "-1.09861228866810969139524523692252570464749e0",
+                "-9.38269638592930308016262357965144709147399e-1",
+                "-7.88457360364270169461184244738941660296105e-1",
+                "-6.46627164925052452438654402400576134462869e-1",
+                "-5.10825623765990683205514096303661934878111e-1",
+                "-3.79489621704903723955539990322534932432111e-1",
+                "-2.51314428280906077685137730401871679657897e-1",
+                "-1.25163142954006014253530047724413191415111e-1",
+                0,
+                "1.25163142954006014253530047724413191415111e-1",
+                "2.51314428280906077685137730401871679657896e-1",
+                "3.79489621704903723955539990322534932432111e-1",
+                "5.10825623765990683205514096303661934878111e-1",
+                "6.46627164925052452438654402400576134462869e-1",
+                "7.88457360364270169461184244738941660296105e-1",
+                "9.38269638592930308016262357965144709147399e-1",
+                "1.09861228866810969139524523692252570464749e0",
+                "1.27296567581288744409616592300919554941412e0",
+                "1.46633706879342704465824220464279290015778e0",
+                "1.68639895357022869958497637754138947441687e0",
+                "1.94591014905531330510535274344317972963709e0",
+                "2.26868354131836433578802679543938590084702e0",
+                "2.70805020110221006599600457014871334417309e0",
+                "3.43398720448514624592916432454235721044994e0"
+            };
+
+            for ((int i, ddouble x) = (0, 1d / 32); i < expecteds.Length; i++, x += 1d / 32) {
+                ddouble expected = expecteds[i];
+
+                ddouble y = ddouble.Logit(x);
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-31d);
+            }
+
+            ddouble log_pzero = ddouble.Logit(0d);
+            ddouble log_pone = ddouble.Logit(1d);
+            ddouble log_ponepeps = ddouble.Logit(ddouble.BitIncrement(1d));
+            ddouble log_mzero = ddouble.Logit(-0d);
+            ddouble log_pinf = ddouble.Logit(double.PositiveInfinity);
+            ddouble log_ninf = ddouble.Logit(double.NegativeInfinity);
+            ddouble log_nan = ddouble.Logit(double.NaN);
+
+            Assert.IsTrue(ddouble.IsNegativeInfinity(log_pzero), nameof(log_pzero));
+            Assert.IsTrue(ddouble.IsPositiveInfinity(log_pone), nameof(log_pone));
+            Assert.IsTrue(ddouble.IsNaN(log_ponepeps), nameof(log_ponepeps));
+            Assert.IsTrue(ddouble.IsNaN(log_mzero), nameof(log_mzero));
+            Assert.IsTrue(ddouble.IsNaN(log_pinf), nameof(log_pinf));
+            Assert.IsTrue(ddouble.IsNaN(log_ninf), nameof(log_ninf));
+            Assert.IsTrue(ddouble.IsNaN(log_nan), nameof(log_nan));
+        }
+
+        [TestMethod]
+        public void ExpitTest() {
+            ddouble[] expecteds = {
+                "1.798620996209155802679313794953842487249e-2",
+                "2.297736991002561495390388669015859139171e-2",
+                "2.931223075135631886528955811418158389418e-2",
+                "3.732688734412946019771416603849814526022e-2",
+                "4.742587317756678087884815177175220138618e-2",
+                "6.008665017400762201972344559490871336252e-2",
+                "7.585818002124355119330617664624777313071e-2",
+                "9.534946489910949396961476322833650130171e-2",
+                "1.192029220221175559402708586976032047936e-1",
+                "1.480471980316894697061848651843231254462e-1",
+                "1.824255238063563403927828213437517524538e-1",
+                "2.22700138825308853000480452338458201971e-1",
+                "2.689414213699951207488407581781637256349e-1",
+                "3.208213008246070268403198842234209787658e-1",
+                "3.775406687981454353610994342544915212467e-1",
+                "4.378234991142018959726763620970536461621e-1",
+                0.5,
+                "5.62176500885798104027323637902946353838e-1",
+                "6.224593312018545646389005657455084787533e-1",
+                "6.791786991753929731596801157765790212342e-1",
+                "7.310585786300048792511592418218362743651e-1",
+                "7.77299861174691146999519547661541798029e-1",
+                "8.175744761936436596072171786562482475462e-1",
+                "8.519528019683105302938151348156768745538e-1",
+                "8.807970779778824440597291413023967952064e-1",
+                "9.046505351008905060303852367716634986983e-1",
+                "9.241418199787564488066938233537522268693e-1",
+                "9.399133498259923779802765544050912866375e-1",
+                "9.525741268224332191211518482282477986138e-1",
+                "9.626731126558705398022858339615018547398e-1",
+                "9.706877692486436811347104418858184161058e-1",
+                "9.770226300899743850460961133098414086083e-1",
+                "9.820137900379084419732068620504615751275e-1"
+            };
+
+            for ((int i, ddouble x) = (0, -4); i < expecteds.Length; i++, x += 1d / 4) {
+                ddouble expected = expecteds[i];
+
+                ddouble y = ddouble.Expit(x);
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-31d);
+            }
+
+            ddouble exp_pinf = ddouble.Expit(double.PositiveInfinity);
+            ddouble exp_ninf = ddouble.Expit(double.NegativeInfinity);
+            ddouble exp_nan = ddouble.Expit(double.NaN);
+
+            Assert.IsTrue(exp_pinf == 1d, nameof(exp_pinf));
+            Assert.IsTrue(ddouble.IsPlusZero(exp_ninf), nameof(exp_ninf));
+            Assert.IsTrue(ddouble.IsNaN(exp_nan), nameof(exp_nan));
         }
     }
 }
