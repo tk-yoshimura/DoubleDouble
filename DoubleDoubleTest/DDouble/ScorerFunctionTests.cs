7c89e1f170b140c08731e2d9f1f0364a54901905
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-12 04:09:13 +0900

    fix: test scorer

--- DoubleDoubleTest/DDouble/ScorerFunctionTests.cs
+++ DoubleDoubleTest/DDouble/ScorerFunctionTests.cs
@@ -5768,9 +5768,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28d);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28d);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28d);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 2e-28d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 2e-28d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 2e-28d);
             }
 
             for ((int i, ddouble x) = (0, -1); i < nz_expecteds.Length; i++, x += 1d / 128) {
@@ -5783,9 +5783,9 @@ namespace DoubleDoubleTest.DDouble {
                 Console.WriteLine(x);
                 Console.WriteLine(y);
 
-                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 1e-28d);
-                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 1e-28d);
-                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 1e-28d);
+                HPAssert.AreEqual(expected, y, ddouble.Abs(expected) * 2e-28d);
+                HPAssert.AreEqual(expected, y_dec, ddouble.Abs(expected) * 2e-28d);
+                HPAssert.AreEqual(expected, y_inc, ddouble.Abs(expected) * 2e-28d);
             }
 
             Assert.IsTrue(ddouble.IsNaN(ddouble.ScorerGi(ddouble.NaN)));
