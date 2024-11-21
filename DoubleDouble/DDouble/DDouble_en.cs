b071dd71b5d6d2fed2e8e39d1b6eb1d2d486044e
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-22 03:16:49 +0900

    fix: en

--- DoubleDouble/DDouble/DDouble_en.cs
+++ DoubleDouble/DDouble/DDouble_en.cs
@@ -1,31 +1,44 @@
-﻿namespace DoubleDouble {
+﻿using Microsoft.VisualBasic;
+using static DoubleDouble.ddouble.Consts;
+
+namespace DoubleDouble {
     public partial struct ddouble {
-        public static ddouble En(ddouble nu, ddouble x) {
-            if (IsNaN(x) || IsNegative(x) || IsNaN(nu) || IsNegative(nu)) {
+        public static ddouble En(int n, ddouble x) {
+            const int max_n = 256, max_iter = 4096;
+
+            if (n < 0 || IsNaN(x) || IsNegative(x)) {
                 return NaN;
             }
 
+            if (n > max_n) {
+                throw new ArgumentOutOfRangeException(
+                    nameof(n),
+                    $"In the calculation of the En function, nu with an absolute value greater than {max_n} is not supported."
+                );
+            }
+
             if (ILogB(x) < -105) {
-                return 1d / (nu - 1d);
+                return Rcp(n - 1);
             }
 
             ddouble t = Exp(-x);
+
+            if (n == 0) {
+                return t / x;
+            }
 
             if (IsZero(t)) {
                 return 0d;
             }
 
             if (x >= 1d) {
-                (ddouble a0, ddouble a1) = (1d, 0d);
-                (ddouble b0, ddouble b1) = (0d, 1d);
-
-                (a0, a1) = (a1, a0 + x * a1);
-                (b0, b1) = (b1, b0 + x * b1);
+                (ddouble a0, ddouble a1) = (0d, 1d);
+                (ddouble b0, ddouble b1) = (1d, x);
 
                 ddouble s = a1 / b1;
 
-                for (int k = 0; k < 4096; k++) {
-                    ddouble c = nu + k;
+                for (int k = 0; k < max_iter; k++) {
+                    double c = n + k;
 
                     (a0, a1) = (a1, c * a0 + a1);
                     (b0, b1) = (b1, c * b0 + b1);
@@ -49,7 +62,33 @@
 
                 return y;
             }
-            else { 
+            else {
+                ddouble c = -Log(x) - EulerGamma;
+                ddouble s = (n != 1) ? Rcp(n - 1) : c;
+                ddouble f = 1d;
+
+                for (int k = 1; k <= max_iter; k++) {
+                    ddouble ds;
+
+                    if (k != n - 1) {
+                        ds = Rcp(n - k - 1);
+                    }
+                    else {
+                        ds = c;
+                        for (int m = 1; m < n; m++) {
+                            ds += Rcp(m);
+                        }
+                    }
+
+                    f *= -x / k;
+                    s = SeriesUtil.Add(s, f, ds, out bool convergence);
+
+                    if (k > n && convergence) {
+                        break;
+                    }
+                }
+
+                return s;
             }
         }
     }
